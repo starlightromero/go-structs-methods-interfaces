57d1318fbfe4295d0284fad31b6a6a8ffc64975b
Starlight Romero <starlightromero@protonmail.com>
2021-04-14 13:36:49 -0700

    [updated] added triangle

--- shapes_test.go
+++ shapes_test.go
@@ -26,6 +26,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10, 10}
 	got := Perimeter(rectangle)
@@ -37,20 +46,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-    
-    areaTests := []struct {
-        shape Shape
-        want  float64
-    }{
-        {Rectangle{12, 6}, 72.0},
-        {Circle{10}, 314.1592653589793},
-    }
 
-    for _, tt := range areaTests {
-        got := tt.shape.Area()
-        if got != tt.want {
-            t.Errorf("got %g want %g", got, tt.want)
-        }
-    }
-    
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+
 }
